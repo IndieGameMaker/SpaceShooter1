e65b36a5c83fd6626686065b922e6e470e6dd574
Jaehyun Lee <myleje@gmail.com>
2019-12-01 16:52:57 +0900

    .

---
+++ Assets/02.Scripts/BarrelCtrl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelCtrl : MonoBehaviour
+{
+    private int hitCount = 0;
+
+    void Start()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("BULLET") == true)
+        {
+            //hitCount = hitCount + 1;  --> ++hitCount;
+            if (++hitCount == 3)
+            {
+                ExpBarrel();
+            }
+        }
+    }
+
+    void ExpBarrel()
+    {
+        Rigidbody rb = this.gameObject.AddComponent<Rigidbody>();
+        rb.AddForce(Vector3.up * 1200.0f);
+    }
+
+}
