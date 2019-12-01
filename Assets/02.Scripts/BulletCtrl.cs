038bf7a3e7097670e61a7cd28121b94f3f52ffd0
Jaehyun Lee <myleje@gmail.com>
2019-12-01 13:55:03 +0900

    .

---
+++ Assets/02.Scripts/BulletCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCtrl : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        //rb = this.gameObject.GetComponent<Rigidbody>();
+
+        rb.AddRelativeForce(Vector3.forward * 500.0f);
+    }
+
+}
