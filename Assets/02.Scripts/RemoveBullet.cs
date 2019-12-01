f36b6a880610814b5dac6fa54e5446461bef20f8
Jaehyun Lee <myleje@gmail.com>
2019-12-01 15:41:07 +0900

    .

--- Assets/02.Scripts/RemoveBullet.cs
+++ Assets/02.Scripts/RemoveBullet.cs
@@ -4,5 +4,12 @@ using UnityEngine;
 
 public class RemoveBullet : MonoBehaviour
 {
-
+    //충돌이 발생했을 때 한번 호출되는 충돌 콜백함수
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.gameObject.tag == "BULLET")
+        {
+            Destroy(coll.gameObject);
+        }
+    }
 }
