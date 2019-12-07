144bfabbd86c9f96a4ef9b2797fe4ff5a58fb1f3
Jaehyun Lee <myleje@gmail.com>
2019-12-07 10:54:03 +0900

    ,

--- Assets/02.Scripts/FireCtrl.cs
+++ Assets/02.Scripts/FireCtrl.cs
@@ -6,17 +6,32 @@ public class FireCtrl : MonoBehaviour
 {
     public GameObject bulletPrefab;
     public Transform firePos;
+    public AudioClip fireSfx;
+
+    private AudioSource source; //AudioSource 컴포넌트를 저장하기 위한 변수
 
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
     }
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0))  //마우스 왼쪽 버튼 클릭할 때 마다
         {
-            Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+            Fire();
         }    
+    }
+
+    void Fire()
+    {
+        //동적으로 총알을 생성하는 로직
+        Instantiate(bulletPrefab, firePos.position, firePos.rotation);
+        //총 발사 음향 실행
+        source.PlayOneShot(fireSfx);
+        /*
+        source.clip = fireSfx;
+        source.Play();
+        */
     }
 }
