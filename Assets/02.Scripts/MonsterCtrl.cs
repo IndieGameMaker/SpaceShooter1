aeb4c8964f0e06766d979b63c6f7986e2df08bae
Jaehyun Lee <myleje@gmail.com>
2019-12-07 13:02:59 +0900

    .

---
+++ Assets/02.Scripts/MonsterCtrl.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterCtrl : MonoBehaviour
+{
+    [SerializeField]
+    private NavMeshAgent agent;
+    [SerializeField]
+    private Transform playerTr;
+
+    private Animator anim;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>(); 
+        anim  = GetComponent<Animator>();
+
+        //하이러키뷰(씬뷰)에 있는 모든 게임오브젝트 중에서 "Player" 이름을 갖는 게임오브젝트를 추출(검색)
+        //Find ~ 계열의 함수는 반드시 Start, Awake 에서만 사용.
+        GameObject playerObj = GameObject.Find("Player");      
+        if (playerObj != null) 
+        {
+            playerTr = playerObj.GetComponent<Transform>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3.Distance(A, B)   A와 B간의 거리를 반환하는 함수
+        if (Vector3.Distance(transform.position, playerTr.position) <= 5.0f)
+        {
+            agent.SetDestination(playerTr.position);
+            agent.isStopped = false; //추적시작
+
+            anim.SetBool("isTrace", true);
+        }
+        else
+        {
+            anim.SetBool("isTrace", false);
+            agent.isStopped = true; //정지
+        }
+    }
+}
